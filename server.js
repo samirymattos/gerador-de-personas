f75b333114094aea54556a616566c1482b59468e
Samiry Mattos <samirymattos@id.uff.br>
2020-03-16 15:36:53 -0300

    iniciando0 node avançado

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
 const app = express();
 let port = 3000;
 
@@ -11,26 +13,7 @@ app.listen(port, () => {
 app.use(bodyParser.json());
 
 const users = {};
-// users["SAMIRY"] = {24, 21/06/1995}
+
 app.get("/", (req, res) => {
   res.json({ users });
-});
-
-app.post("/", (req, res) => {
-  const { nome, sobrenome, idade, nascimento } = req.body;
-  users[nome] = { sobrenome, nascimento, idade };
-  res.json({ msg: "usuário criado com sucesso" });
-});
-
-app.put("/:nome", (req, res) => {
-  const { nome } = req.params;
-  const { sobrenome, nascimento, idade } = req.body;
-  users[nome] = { sobrenome, nascimento, idade };
-  res.json({ msg: "usuário alterado com sucesso" });
-});
-
-app.delete("/:nome", (req, res) => {
-  const { nome } = req.params;
-  delete users[nome];
-  res.json({ msg: "usuário deletado com sucesso" });
 });
