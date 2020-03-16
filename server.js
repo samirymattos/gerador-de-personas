ab54d7eed79f6498cb536407708c06be995e33b2
Samiry Mattos <samirymattos@id.uff.br>
2020-03-16 15:36:18 -0300

    nodejs iniciante

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-
 let port = 3000;
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
 
-const user = {};
+app.use(bodyParser.json());
 
+const users = {};
+// users["SAMIRY"] = {24, 21/06/1995}
 app.get("/", (req, res) => {
-  res.send("Usando get");
+  res.json({ users });
 });
 
 app.post("/", (req, res) => {
-  res.send("Usando post");
+  const { nome, sobrenome, idade, nascimento } = req.body;
+  users[nome] = { sobrenome, nascimento, idade };
+  res.json({ msg: "usuário criado com sucesso" });
 });
 
-app.put("/", (req, res) => {
-  res.send("Usando put");
+app.put("/:nome", (req, res) => {
+  const { nome } = req.params;
+  const { sobrenome, nascimento, idade } = req.body;
+  users[nome] = { sobrenome, nascimento, idade };
+  res.json({ msg: "usuário alterado com sucesso" });
 });
 
-app.delete("/", (req, res) => {
-  res.send("Usando delete");
+app.delete("/:nome", (req, res) => {
+  const { nome } = req.params;
+  delete users[nome];
+  res.json({ msg: "usuário deletado com sucesso" });
 });
