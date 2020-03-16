63a28bedb9b0b4240868052fcbfa8c0f39b7f57b
Samiry Mattos <samirymattos@id.uff.br>
2020-03-16 09:54:56 -0300

    iniciando verbos HTTP

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
 
+const user = {};
+
 app.get("/", (req, res) => {
-  res.send("Minha primeira rota");
+  res.send("Usando get");
+});
+
+app.post("/", (req, res) => {
+  res.send("Usando post");
+});
+
+app.put("/", (req, res) => {
+  res.send("Usando put");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Usando delete");
 });
