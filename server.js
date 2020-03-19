8ed82ed144488bf21b8d0b5707cc1a1fd8d56d9f
bell <samirymattos@gmail.com>
2020-03-19 13:51:55 -0300

    peparando terreno

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
