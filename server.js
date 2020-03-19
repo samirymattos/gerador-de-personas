4361be5ed326e5654f9ddb9b74d3039bcc83cb9c
bell <samirymattos@gmail.com>
2020-03-19 13:43:16 -0300

    cors adicionado

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 
@@ -16,5 +17,6 @@ mongoose.connect(
 );
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/", require("./src/routes.js"));
